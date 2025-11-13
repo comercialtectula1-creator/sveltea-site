e45c28f6803c2a2b764ebc699171297ded757d0a
comercialtectula1-creator <comercialtectula1@gmail.com>
2025-11-13 10:11:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const PRODUCTS = [
   {
     id: "p01",
     name: "Sveltea Esencia",
-    price: 399,
+    price: 100,
     short: "Refresca y revitaliza la piel con extractos naturales.",
     image: "Escencia.jpeg",
     link: "product.html?id=p01"
   },
   {
     id: "p02",
-    name: "Sveltea Renova 30 cápsulas",
-    price: 699,
+    name: "Sveltea Renova",
+    price: 1200,
     short: "Regenera desde adentro. Colágeno, antioxidantes y vitaminas.",
     image: "Renova.jpeg",
     link: "product.html?id=p02"
@@ -18,7 +18,7 @@ const PRODUCTS = [
   {
     id: "p03",
     name: "Sveltea Silueta",
-    price: 549,
+    price: 1400,
     short: "Apoya el metabolismo y la definición corporal de forma natural.",
     image: "Silueta.jpeg",
     link: "product.html?id=p03"
@@ -26,7 +26,7 @@ const PRODUCTS = [
   {
     id: "p04",
     name: "Sveltea Define",
-    price: 599,
+    price: 1000,
     short: "Moldea, tonifica y resalta tu figura con ingredientes selectos.",
     image: "Define.jpeg",
     link: "product.html?id=p04"
